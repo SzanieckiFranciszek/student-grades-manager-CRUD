46b433ea0fcd366d7cb4b48e6ee55cd76371e6f5
Franciszek Szaniecki <franekszaniecki@gmail.com>
2023-05-20 13:43:52 +0200

    development/initial commit with setting for project

---
+++ src/main/java/com/franciszekszaniecki/studentgradesmanager/StudentGradesManagerApplication.java
@@ -0,0 +1,13 @@
+package com.franciszekszaniecki.studentgradesmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentGradesManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentGradesManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/franciszekszaniecki/studentgradesmanager/StudentGradesManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franciszekszaniecki.studentgradesmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentGradesManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
